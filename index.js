430593bd87798388fea7fba68ea134bbacf4e46c
William Russell <williamrussell@Williams-MBP.lan>
2023-02-04 23:30:29 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Write your code here!
+
+const problem = document.querySelector('#main');
+
+problem.remove();
+
+const newHeader = document.createElement('h1');
+
+
+
+newHeader.id = 'victory';
+
+newHeader.textContent = 'Bill Russell is the champion';
+
+document.body.append(newHeader);
